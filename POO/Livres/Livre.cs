1849fead13cbc2297be107af8c5d5d385ac91086
Nabsyle <39308506+Nabsyle@users.noreply.github.com>
2018-05-15 23:08:42 +0200

    Add files via upload

---
+++ POO/Livres/Livre.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Livre
+    {
+        public string Titre;
+        public string Auteur;
+        public string ISBN;
+        public int AnParution;
+        public int Tirage;
+        public int CoutProduction;
+        public int PrixVente;
+        public int NombrePages;
+        public int Poids;
+        public int StockEncours;
+
+        public void AugmenterTirage()
+        {
+            this.Tirage++;
+            this.StockEncours++;
+            Console.WriteLine("Le nombre d'exemplaires est {0} ", Tirage);
+        }
+
+        public void CalculerMarge()
+        {
+            //Todo
+        }
+        public void MettreAuPilon()
+        {
+            StockEncours--;
+            Console.WriteLine("Stock en cours: {0}", StockEncours);
+        }
+
+    }
+}
---
+++ POO/Livres/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livre monLivre = new Livre();
+            monLivre.Titre = "Apprendre C# en s'amusant";
+            monLivre.Auteur = "Claude C.";
+            monLivre.ISBN = "978-1-4717-4645-1";
+            monLivre.AnParution = 2014;
+            monLivre.Tirage = 20;
+            monLivre.CoutProduction = 7;
+            monLivre.PrixVente = 15;
+            monLivre.NombrePages = 177;
+            monLivre.Poids = 320;
+            monLivre.StockEncours = 10;
+
+            Console.WriteLine("Tirage du livre \"{0}\" en {1} exemplaires", monLivre.Titre, monLivre.Tirage);
+
+            Console.WriteLine("Stock en cours du livre \"{0}\" : {1} ", monLivre.Titre, monLivre.StockEncours);
+            monLivre.AugmenterTirage();
+            Console.WriteLine("Nouveau stock en cours du livre \"{0}\" : {1} ", monLivre.Titre, monLivre.StockEncours);
+            Console.ReadKey();
+        }
+    }
+}
